bc3f5a626a720807995faf0e81124b1a67fd1b4e
Rimpy99 <70006pawel@gmail.com>
2024-03-23 23:30:56 +0100

    2303-pc3

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 import { PIZZA_DATA } from './pizzaData.js';
 
+//elementy do okna wyboru opcji do pizzy
 const addPizzaButtons = Array.from(document.querySelectorAll('.add-pizza-button'));
-const modalBackground = document.querySelector('#pizza-modal-background');
+const addPizzaModalBackground = document.querySelector('#pizza-modal-background');
 const addPizzaModal = document.querySelector('#pizza-modal');
-const elementsToCloseModal = Array.from(document.querySelectorAll('.pizza-modal-close'));
+const elementsToClosePizzaModal = Array.from(document.querySelectorAll('.pizza-modal-close'));
 const addToOrdersBtn = document.querySelector('#add-to-orders-btn');
-const basketOrderCounter = document.querySelector('#basket-order-counter');
+const basketOrderCounter = document.querySelector('#basket-orders-counter');
+//elementy do okna zamówienia
+const basketBtn = document.querySelector('#basket-orders-counter');
+const ordersModal = document.querySelector('#orders-modal');
+const ordersModalBackground = document.querySelector('#orders-modal-background');
+const elementsToCloseOrdersModal = Array.from(document.querySelectorAll('.orders-modal-close'));
 
 const changePriceFormat = (price) => price.toString().replace('.', ',');
 
-const orders = [];
+let orders = [];
 let currentPizzaId = 0;
 
 //do otwierania okna z wyborem opcji do pizzy
@@ -18,7 +24,7 @@ addPizzaButtons.forEach(btn => {
         currentPizzaId = parseInt(btn.value[btn.value.length - 1]);
         const selectedPizzaData = PIZZA_DATA[currentPizzaId];
         
-        modalBackground.style.display = 'block';
+        addPizzaModalBackground.style.display = 'block';
 
         document.querySelector('#pizza-modal-name').textContent = selectedPizzaData.name;
         document.querySelector('#pizza-modal-desc').textContent = selectedPizzaData.desc;
@@ -27,23 +33,23 @@ addPizzaButtons.forEach(btn => {
 
         addPizzaModal.style.display = 'block';
 
-        document.addEventListener('keydown', (event) => event.key == 'Escape' && closeModal());        
+        document.addEventListener('keydown', (event) => event.key == 'Escape' && closePizzaModal());        
     });
 });
 
 //do zamykania okna z wyborem opcji do pizzy
-const closeModal = () => {
-    modalBackground.style.display = 'none';
+const closePizzaModal = () => {
+    addPizzaModalBackground.style.display = 'none';
     addPizzaModal.style.display = 'none';
     setPizzaModalToDefault();
 };
 
 //kiedy się kliknie na jakikolwiek element, który służy do zamykania okna
-elementsToCloseModal.forEach(elem => elem.addEventListener('click', () => closeModal()));
+elementsToClosePizzaModal.forEach(elem => elem.addEventListener('click', () => closePizzaModal()));
 
 //do ustawienia ponownie wartości na początkowe po opuszczeniu okna z wyborem opcji do pizzy
 const setPizzaModalToDefault = () => {
-    document.querySelector('.pizza-modal-select').value = "normal";
+    document.querySelector('.pizza-modal-select').value = 'normal';
     document.querySelector('#tomato-sauce-checkbox').checked = false;
     document.querySelector('#garlic-sauce-checkbox').checked = false;
 }
@@ -63,6 +69,20 @@ addToOrdersBtn.addEventListener('click', () => {
 
     basketOrderCounter.innerText = orders.length;
 
-    closeModal();
+    closePizzaModal();
     setPizzaModalToDefault();
 });
+
+//do otwarcia okna z zamówieniem
+basketBtn.addEventListener('click', () => {
+    ordersModalBackground.style.display = 'block';
+    ordersModal.style.display = 'block';
+    document.addEventListener('keydown', (event) => event.key == 'Escape' && closeOrderModal()); 
+});
+
+const closeOrderModal = () => {
+    ordersModalBackground.style.display = 'none';
+    ordersModal.style.display = 'none';
+}
+
+elementsToCloseOrdersModal.forEach(elem => elem.addEventListener('click', () => closeOrderModal()));
